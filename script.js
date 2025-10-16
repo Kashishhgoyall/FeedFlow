a8d1d47deab2f1f8baac7df2231192fdf010e86a
Kashish <goyalkashish439@gmail.com>
2025-10-16 17:26:48 +0530

    Updated style.css(cards + feed), index.html(feed container), script.js(infinite scroll)

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("FeedFlow project initialized successfully!");
+const feed = document.getElementById("feed");
+
+
+function generatePosts(count = 6) {
+  for (let i = 0; i < count; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+      <img src="https://picsum.photos/seed/${Math.random()}/550/300" alt="Post Image">
+      <div class="card-content">
+        <h3>FeedFlow Post #${Math.floor(Math.random() * 500)}</h3>
+        <p>This is a sample post </p>
+      </div>
+    `;
+    feed.appendChild(card);
+  }
+}
+
+
+generatePosts();
+
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 10) {
+    generatePosts(4);
+  }
+});
